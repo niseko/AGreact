5963c4eaf27c6bcb51ae54042f1f98f3cc5d6cc3
niseko <rentz@hotmail.de>
2019-01-21 16:29:37 +0100

    Updates

---
+++ precache-manifest.e8b337c8da070e9e4b8067a73a4d40fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f76c757e8772f77ccf4",
+    "url": "/AGreact/static/js/runtime~main.7f76c757.js"
+  },
+  {
+    "revision": "ad7415d074ac426922dd",
+    "url": "/AGreact/static/js/main.ad7415d0.chunk.js"
+  },
+  {
+    "revision": "314ec8d546b30289379f",
+    "url": "/AGreact/static/js/1.314ec8d5.chunk.js"
+  },
+  {
+    "revision": "ad7415d074ac426922dd",
+    "url": "/AGreact/static/css/main.f4ffd2fd.chunk.css"
+  },
+  {
+    "revision": "66be2b79b63297bdfbcd2f7e71fdf41d",
+    "url": "/AGreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/agreact/precache-manifest.38458db5b9fc55efa331c093dccd4a85.js"
+  "/AGreact/precache-manifest.e8b337c8da070e9e4b8067a73a4d40fa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/agreact/index.html", {
+workbox.routing.registerNavigationRoute("/AGreact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
